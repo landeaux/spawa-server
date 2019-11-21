6bf3f377f085f54e04e0506b596fd16a41739a19
landeaux <pluckedowl@gmail.com>
2019-11-21 08:29:42 -0800

    add uniqueness validation to username and email fields of User schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,18 +1,21 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var UserSchema = new mongoose.Schema({
   username: {
-    type: String, 
-    lowercase: true, 
-    required: [ true, "can't be blank" ], 
-    match: [ /^[a-zA-Z0-9]+$/, 'is invalid' ], 
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [ true, "can't be blank" ],
+    match: [ /^[a-zA-Z0-9]+$/, 'is invalid' ],
     index: true // optimize queries that use this field
   },
   email: {
-    type: String, 
-    lowercase: true, 
-    required: [ true, "can't be blank" ], 
-    match: [ /\S+@\S+\.\S+/, 'is invalid' ], 
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [ true, "can't be blank" ],
+    match: [ /\S+@\S+\.\S+/, 'is invalid' ],
     index: true // optimize queries that use this field
   },
   bio: String,
@@ -20,6 +23,9 @@ var UserSchema = new mongoose.Schema({
   hash: String,
   salt: String
 }, { timestamps: true }); // this option creates createdAt and updatedAt fields
+
+// validate uniqueness of fields with "unique: true" option
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 
 // register the schema within mongoose
 mongoose.model('User', UserSchema);

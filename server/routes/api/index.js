7e30eb644713b164ff81850d57bd0a121dc5b561
landeaux <pluckedowl@gmail.com>
2019-11-23 10:53:41 -0800

    add api route for profiles

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,10 +1,11 @@
 var router = require('express').Router();
 
 router.use('/', require('./users'));
+router.use('/profiles', require('./profiles'));
 router.use('/posts', require('./posts'));
 
 router.use(function (err, req, res, next) {
-  if(err.name === 'ValidationError') {
+  if (err.name === 'ValidationError') {
     return res.status(422).json({
       errors: Object.keys(err.errors).reduce(function (errors, key) {
         errors[key] = err.errors[key].message;

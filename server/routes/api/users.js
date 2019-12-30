77bbe5ffdef743919908d1b87d6b1a7dc7be9e67
landeaux <pluckedowl@gmail.com>
2019-12-30 11:49:52 -0800

    reorganize user routes

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,19 +2,19 @@ const router = require('express').Router();
 const auth = require('../auth');
 const userController = require('../../controllers/userController');
 
-// Get all users
-router.get('/users', auth.required, userController.getUsers);
+// Create user
+router.post('/users', userController.signUp);
+
+// Authenticate User
+router.post('/users/login', userController.login);
 
 // Get user by ID
 router.get('/user', auth.required, userController.getUser);
 
+// Get all users
+router.get('/users', auth.required, userController.getUsers);
+
 // Update user
 router.put('/user', auth.required, userController.updateUser);
-
-// Login
-router.post('/users/login', userController.login);
-
-// Create user
-router.post('/users', userController.signUp);
 
 module.exports = router;

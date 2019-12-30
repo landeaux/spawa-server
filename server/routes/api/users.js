44429a0ec318f4373210e93717791db673fc98bf
landeaux <pluckedowl@gmail.com>
2019-12-30 11:46:33 -0800

    import userController and use controller methods from there in place of anonymous functions

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,82 +1,20 @@
-const mongoose = require('mongoose');
 const router = require('express').Router();
-const passport = require('passport');
 const auth = require('../auth');
-
-const User = mongoose.model('User');
+const userController = require('../../controllers/userController');
 
 // Get all users
-router.get('/users', auth.required, (req, res, next) => {
-  User.find({})
-    .then((users) => res.status(200).json({ users: users.map((user) => user.toUserJSONFor()) }))
-    .catch(next);
-});
+router.get('/users', auth.required, userController.getUsers);
 
 // Get user by ID
-router.get('/user', auth.required, (req, res, next) => {
-  User.findById(req.payload.id).then((user) => {
-    if (!user) { return res.sendStatus(401); }
-
-    return res.json({ user: user.toAuthJSON() });
-  }).catch(next);
-});
+router.get('/user', auth.required, userController.getUser);
 
 // Update user
-router.put('/user', auth.required, (req, res, next) => {
-  User.findById(req.payload.id).then((user) => {
-    if (!user) { return res.sendStatus(401); }
-
-    // only update fields that were actually passed...
-    if (typeof req.body.user.username !== 'undefined') {
-      user.username = req.body.user.username;
-    }
-    if (typeof req.body.user.email !== 'undefined') {
-      user.email = req.body.user.email;
-    }
-    if (typeof req.body.user.bio !== 'undefined') {
-      user.bio = req.body.user.bio;
-    }
-    if (typeof req.body.user.image !== 'undefined') {
-      user.image = req.body.user.image;
-    }
-    if (typeof req.body.user.password !== 'undefined') {
-      user.setPassword(req.body.user.password);
-    }
-
-    return user.save().then(() => res.json({ user: user.toAuthJSON() }));
-  }).catch(next);
-});
+router.put('/user', auth.required, userController.updateUser);
 
 // Login
-router.post('/users/login', (req, res, next) => {
-  if (!req.body.user.email) {
-    return res.status(422).json({ errors: { email: 'can\'t be blank' } });
-  }
-
-  if (!req.body.user.password) {
-    return res.status(422).json({ errors: { password: 'can\'t be blank' } });
-  }
-
-  return passport.authenticate('local', { session: false }, (err, user, info) => {
-    if (err) { return next(err); }
-
-    if (user) {
-      user.token = user.generateJWT();
-      return res.json({ user: user.toAuthJSON() });
-    }
-    return res.status(422).json(info);
-  })(req, res, next);
-});
+router.post('/users/login', userController.login);
 
 // Create user
-router.post('/users', (req, res, next) => {
-  const user = new User();
-
-  user.username = req.body.user.username;
-  user.email = req.body.user.email;
-  user.setPassword(req.body.user.password);
-
-  user.save().then(() => res.json({ user: user.toAuthJSON() })).catch(next);
-});
+router.post('/users', userController.signUp);
 
 module.exports = router;

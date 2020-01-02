a14ba2c8bb3cd969dbce73d778792229978365ff
landeaux <pluckedowl@gmail.com>
2020-01-01 20:14:13 -0800

    move grantAccess into its own accessController.js file since it will be shared between all controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,22 +1,7 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
-const { roles } = require('../roles');
 
 const User = mongoose.model('User');
-
-exports.grantAccess = (action, resource) => async (req, res, next) => {
-  try {
-    const permission = roles.can(req.payload.role)[action](resource);
-    if (!permission.granted) {
-      return res.status(401).json({
-        error: 'You don\'t have enough permission to perform this action',
-      });
-    }
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-};
 
 // User signup
 exports.signup = async (req, res, next) => {

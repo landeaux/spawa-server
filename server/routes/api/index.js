733571c0548da502d35f8ee2fe7b8c0e11d49a3c
Cody <cody_ryan25@hotmail.com>
2020-01-16 17:50:39 -0800

    actually added the mods

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.use('/', require('./users'));
 router.use('/profiles', require('./profiles'));
-router.use('/posts', require('./posts'));
+
 
 router.use((err, req, res, next) => {
   if (err.name === 'ValidationError') {

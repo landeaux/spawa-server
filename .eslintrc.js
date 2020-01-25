ad839d9bb9d5c8b624bb7bdb9281809721a5378e
landeaux <pluckedowl@gmail.com>
2020-01-25 15:00:36 -0800

    Add some extra eslint rules
    - no-multiple-empty-lines: no more than one empty line should be allowed
    - no-underscore-dangle: add allow rule for '_id' for mongodb ids

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-param-reassign': ['error', {'props': false}]
+    'no-param-reassign': ['error', { 'props': false }],
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1 }],
+    'no-underscore-dangle': ['error', { 'allow': ['_id'] }] // for mongodb _id
   },
   ignorePatterns: ['node_modules/', 'server/public', 'client/']
 };

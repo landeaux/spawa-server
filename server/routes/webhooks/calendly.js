3e3b4b0bc30e1c99509f68605fb1315247d93f70
landeaux <pluckedowl@gmail.com>
2020-01-29 14:54:19 -0800

    Update /invitee-created route handler
    The /initee-created route handler was updated to grab the invitee's
    email from the req.body, find the user in the database by id, and
    push the event to the user's events prop.

--- server/routes/webhooks/calendly.js
+++ server/routes/webhooks/calendly.js
@@ -1,11 +1,27 @@
+const mongoose = require('mongoose');
 const router = require('express').Router();
 
+const User = mongoose.model('User');
+
 // invitee.created
-router.post('/invitee-created', (req, res, next) => {
-  console.log('calendly webhook route hit');
-  console.log('req.body:', req.body);
-  res.sendStatus(200);
-  next();
+router.post('/invitee-created', async (req, res, next) => {
+  try {
+    if (req.body.event !== 'invitee.created') {
+      res.sendStatus(400);
+    } else {
+      const { email } = req.body.payload.invitee;
+      const { event } = req.body.payload;
+      const user = await User.findOne({ email });
+      if (user) {
+        user.events.push(event);
+        await user.save();
+      }
+      res.sendStatus(200);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 // invitee.canceled

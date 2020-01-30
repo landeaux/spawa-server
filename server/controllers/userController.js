2b2904b8909ee1b8b0367f052ced0ffff0bd701f
Cody <cody_ryan25@hotmail.com>
2020-01-29 18:46:52 -0800

    Second part of issue complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -87,6 +87,7 @@ exports.getUserById = async (req, res, next) => {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) return res.sendStatus(400);
     const user = await User.findById(id);
+    user.avaiable = req.body.user.avaiable;
     return user
       ? res.status(200).json({ user: user.toUserJSONFor() }) // user found
       : res.sendStatus(404); // user not found

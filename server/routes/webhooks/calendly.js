6903de905904c95b431e309dddf60d15b1f6cd34
landeaux <pluckedowl@gmail.com>
2020-01-29 17:54:01 -0800

    Update invitee-created route handler
    - The new Event mongoose model is used to store event data from the
      webhook request. This data is a blend of some values from the
      event data and event_type data in the request payload.

--- server/routes/webhooks/calendly.js
+++ server/routes/webhooks/calendly.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const router = require('express').Router();
 
 const User = mongoose.model('User');
+const Event = mongoose.model('Event');
 
 // invitee.created
 router.post('/invitee-created', async (req, res, next) => {
@@ -9,12 +10,18 @@ router.post('/invitee-created', async (req, res, next) => {
     if (req.body.event !== 'invitee.created') {
       res.sendStatus(400);
     } else {
+      const eventTypeData = req.body.payload.event_type;
+      const eventData = req.body.payload.event;
+      const allEventData = Object.assign(eventTypeData, eventData);
+
+      const event = new Event(allEventData);
+      await event.save();
+
       const { email } = req.body.payload.invitee;
-      const { event } = req.body.payload;
       const user = await User.findOne({ email });
       if (user) {
-        user.events.push(event);
-        await user.save();
+        // #todo Push the event instance to the user's events prop
+        // await user.save();
       }
       res.sendStatus(200);
     }

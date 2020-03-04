510760b3aa13c848b82984180fba950584501757
Cody <cody_ryan25@hotmail.com>
2020-03-03 16:42:59 -0800

    everything except for owner populated

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.use('/', require('./users'));
 router.use('/profile', require('./profiles'));
+router.use('/', require('./pitchDecks'));
 
 router.use((err, req, res, next) => {
   if (err.name === 'ValidationError') {

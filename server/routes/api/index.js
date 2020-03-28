4e0502092fe9748c655aa1b5d95c1518c535acf9
landeaux <pluckedowl@gmail.com>
2020-03-28 15:24:49 -0700

    Remove contacts api
    - Remove contacts api since the functionality has now become part of
      the profiles api

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,7 +4,6 @@ router.use('/', require('./users'));
 router.use('/profile', require('./profiles'));
 router.use('/', require('./pitchDecks'));
 router.use('/', require('./reviews'));
-router.use('/contacts', require('./contacts'));
 
 router.use((err, req, res, next) => {
   if (err.name === 'ValidationError') {

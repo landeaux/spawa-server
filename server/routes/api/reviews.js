bb2d0eca70d55a74909900e17a770d23bbf81dae
landeaux <pluckedowl@gmail.com>
2020-04-05 16:13:53 -0700

    Add grantOwnerAccess middleware to routes
    - Implement grantOwnerAccess middleware function which checks if the
      requesting user has ownership of the resource before granting access
      to the route.
    - Use grantOwnerAccess in GET /api/reviews/:id and GET /api/reviews
      routes

--- server/routes/api/reviews.js
+++ server/routes/api/reviews.js
@@ -1,22 +1,50 @@
+const mongoose = require('mongoose');
 const router = require('express').Router();
-
 const {
   createReview,
   getReviewById,
   getReviews,
   deleteReview,
 } = require('../../controllers/reviewController');
+const auth = require('../auth');
+const { grantAccess } = require('../../controllers/accessController');
+const { roles } = require('../../roles');
+
+const User = mongoose.model('User');
+
+const grantOwnerAccess = (action, resource) => async (req, res, next) => {
+  try {
+    const userId = req.payload.id;
+    if (!mongoose.Types.ObjectId.isValid(userId)) return res.sendStatus(400);
+    const user = await User.findById(userId);
+    if (!user) return req.sendStatus(400);
+    const { reviews } = user;
+    const isOwner = reviews.includes(mongoose.Types.ObjectId(req.params.id));
+    const possession = isOwner
+      ? 'Own'
+      : 'Any';
+    const permission = roles.can(req.payload.role)[`${action}${possession}`](resource);
+    if (!permission.granted) {
+      return res.status(401).json({
+        error: 'You don\'t have enough permission to perform this action',
+      });
+    }
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
 
 // Create review
-router.post('/reviews', createReview);
+router.post('/reviews', auth.required, grantAccess('createOwn', 'review'), createReview);
 
 // Get review by ID
-router.get('/reviews/:id', getReviewById);
+router.get('/reviews/:id', auth.required, grantOwnerAccess('read', 'review'), getReviewById);
 
 // Get all reviews
-router.get('/reviews', getReviews);
+router.get('/reviews', auth.required, grantAccess('readAny', 'review'), getReviews);
 
 // Delete review
-router.delete('/reviews/:id', deleteReview);
+router.delete('/reviews/:id', auth.required, grantOwnerAccess('delete', 'review'), deleteReview);
 
 module.exports = router;

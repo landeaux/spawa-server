e145ee40e3a150584459845b1bf53d66c1222d67
landeaux <pluckedowl@gmail.com>
2020-04-05 17:13:47 -0700

    Add updated grantOwnerAccess middleware
    Move grantOwnerAccess middleware from reviews api file to
    accessController and update it to be generalized for other types of
    resources.

--- server/controllers/accessController.js
+++ server/controllers/accessController.js
@@ -1,8 +1,44 @@
+const mongoose = require('mongoose');
 const { roles } = require('../roles');
+
+const User = mongoose.model('User');
 
 exports.grantAccess = (action, resource) => async (req, res, next) => {
   try {
     const permission = roles.can(req.payload.role)[action](resource);
+    if (!permission.granted) {
+      return res.status(401).json({
+        error: 'You don\'t have enough permission to perform this action',
+      });
+    }
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+exports.grantOwnerAccess = (action, resource) => async (req, res, next) => {
+  try {
+    let isOwner = false;
+    if (resource === 'review' || resource === 'pitchdeck') {
+      const userId = req.payload.id;
+      if (!mongoose.Types.ObjectId.isValid(userId)) return res.sendStatus(400);
+      const user = await User.findById(userId);
+      if (!user) return req.sendStatus(400);
+      if (resource === 'review') {
+        const { reviews } = user;
+        isOwner = reviews.includes(mongoose.Types.ObjectId(req.params.id));
+      } else if (resource === 'pitchdeck') {
+        const { pitchDeck } = user;
+        isOwner = pitchDeck.toString() === req.params.id;
+      }
+    } else if (resource === 'user') {
+      isOwner = req.payload.id === req.params.id;
+    }
+    const possession = isOwner
+      ? 'Own'
+      : 'Any';
+    const permission = roles.can(req.payload.role)[`${action}${possession}`](resource);
     if (!permission.granted) {
       return res.status(401).json({
         error: 'You don\'t have enough permission to perform this action',

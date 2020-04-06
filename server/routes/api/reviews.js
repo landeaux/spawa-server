024cc765c710d3745e55c8d669237e142ac55096
landeaux <pluckedowl@gmail.com>
2020-04-05 17:12:27 -0700

    Move grantOwnerAccess to accessController

--- server/routes/api/reviews.js
+++ server/routes/api/reviews.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const router = require('express').Router();
 const {
   createReview,
@@ -7,33 +6,7 @@ const {
   deleteReview,
 } = require('../../controllers/reviewController');
 const auth = require('../auth');
-const { grantAccess } = require('../../controllers/accessController');
-const { roles } = require('../../roles');
-
-const User = mongoose.model('User');
-
-const grantOwnerAccess = (action, resource) => async (req, res, next) => {
-  try {
-    const userId = req.payload.id;
-    if (!mongoose.Types.ObjectId.isValid(userId)) return res.sendStatus(400);
-    const user = await User.findById(userId);
-    if (!user) return req.sendStatus(400);
-    const { reviews } = user;
-    const isOwner = reviews.includes(mongoose.Types.ObjectId(req.params.id));
-    const possession = isOwner
-      ? 'Own'
-      : 'Any';
-    const permission = roles.can(req.payload.role)[`${action}${possession}`](resource);
-    if (!permission.granted) {
-      return res.status(401).json({
-        error: 'You don\'t have enough permission to perform this action',
-      });
-    }
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-};
+const { grantAccess, grantOwnerAccess } = require('../../controllers/accessController');
 
 // Create review
 router.post('/reviews', auth.required, grantAccess('createOwn', 'review'), createReview);

317bd0c91f3583c270f1e7d445f5efb4839e3249
landeaux <pluckedowl@gmail.com>
2020-04-06 23:50:48 -0700

    Generate Key from filename + uuid
    AWS will overwrite files with the same name, but we don't want that, so
    we create the Key from the filename + uuid so Keys are guaranteed unique
    and won't be overwritten.

--- server/controllers/aws.controller.js
+++ server/controllers/aws.controller.js
@@ -1,10 +1,14 @@
+const uuid = require('uuid');
 const s3 = require('../config/s3.config.js');
 
 exports.doUpload = (req, res, next) => {
   const { s3Client } = s3;
   const params = s3.uploadParams;
 
-  params.Key = req.file.originalname;
+  const fileName = req.file.originalname;
+  const fileNameNoExt = fileName.substr(0, fileName.lastIndexOf('.'));
+  const ext = fileName.substr(fileName.lastIndexOf('.') + 1);
+  params.Key = `${fileNameNoExt}_${uuid.v4()}.${ext}`;
   params.Body = req.file.buffer;
 
   s3Client.upload(params, (err, response) => {

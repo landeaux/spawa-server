4d4cb69a754555adfac5b621235502ae0793a55a
landeaux <pluckedowl@gmail.com>
2020-04-10 20:38:57 -0700

    Update doUpload and doDownload headers
    Update headers to match the content being uploaded or downloaded

--- server/controllers/aws.controller.js
+++ server/controllers/aws.controller.js
@@ -8,8 +8,13 @@ exports.doUpload = (req, res, next) => {
   const fileName = req.file.originalname;
   const fileNameNoExt = fileName.substr(0, fileName.lastIndexOf('.'));
   const ext = fileName.substr(fileName.lastIndexOf('.') + 1);
+
   params.Key = `${fileNameNoExt}_${uuid.v4()}.${ext}`;
   params.Body = req.file.buffer;
+  params.ContentType = {
+    pdf: 'application/pdf',
+    pptx: 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+  }[ext];
 
   s3Client.upload(params, (err, response) => {
     if (err) return next(err);
@@ -23,8 +28,17 @@ exports.doDownload = (req, res) => {
   const params = s3.downloadParams;
 
   params.Key = req.params.key;
+  const { filename } = req.params;
 
   s3Client.getObject(params)
+    .on('httpHeaders', (statusCode, httpHeaders) => {
+      if (statusCode === 200) {
+        res.set({
+          'Content-Type': httpHeaders['content-type'],
+          'Content-Disposition': `attachment; filename*=utf-8''${encodeURI(filename)}`,
+        });
+      }
+    })
     .createReadStream()
     .on('error', (err) => {
       res.status(500).json({ error: `Error -> ${err}` });

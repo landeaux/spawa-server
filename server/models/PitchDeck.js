623049e17e023a981a2906ea09268e4576dfa908
landeaux <pluckedowl@gmail.com>
2020-04-27 23:34:27 -0700

    Various updates to PitchDeck model
    - add attemptsLeft to toJSON method
    - add getters for each status
    - update isLocked method to also check status and attemptsLeft

--- server/models/PitchDeck.js
+++ server/models/PitchDeck.js
@@ -90,6 +90,7 @@ PitchDeckSchema.methods.toPitchDeckJSON = function toPitchDeckJSON() {
     owner: this.owner,
     lockDate: this.lockDate,
     isLocked: this.isLocked(),
+    attemptsLeft: this.attemptsLeft,
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
     s3Key,
@@ -153,19 +154,55 @@ PitchDeckSchema.methods.decrementAttemptsLeft = function decrementAttemptsLeft()
 };
 
 /**
- * Checks to see if the pitch deck is locked for new uploads
+ * Checks to see if the pitch deck is locked for new uploads. True if
+ * current date is past the lock date or in a non-editable state or zero
+ * attempts to resubmit are left.
  * @returns {boolean}
  */
 PitchDeckSchema.methods.isLocked = function isLocked() {
-  return (this.lockDate && this.lockDate < Date.now());
+  return (this.lockDate && this.lockDate < Date.now())
+      || (this.isUnderReview() || this.isAccepted() || this.isRejected())
+      || (this.attemptsLeft === 0);
+};
+
+/**
+ * Checks to see if the pitch deck is not ready for review
+ * @returns {boolean}
+ */
+PitchDeckSchema.methods.isNotReady = function isNotReady() {
+  return this.status === NOT_READY;
 };
 
 /**
  * Checks to see if the pitch deck is under review (i.e. active)
  * @returns {boolean}
  */
-PitchDeckSchema.methods.isUnderReview = function isLocked() {
+PitchDeckSchema.methods.isUnderReview = function isUnderReview() {
   return this.status === UNDER_REVIEW;
+};
+
+/**
+ * Checks to see if the pitch deck needs rework
+ * @returns {boolean}
+ */
+PitchDeckSchema.methods.isNeedsRework = function isNeedsRework() {
+  return this.status === NEEDS_REWORK;
+};
+
+/**
+ * Checks to see if the pitch deck is accepted
+ * @returns {boolean}
+ */
+PitchDeckSchema.methods.isAccepted = function isAccepted() {
+  return this.status === ACCEPTED;
+};
+
+/**
+ * Checks to see if the pitch deck is rejected
+ * @returns {boolean}
+ */
+PitchDeckSchema.methods.isRejected = function isRejected() {
+  return this.status === REJECTED;
 };
 
 /**

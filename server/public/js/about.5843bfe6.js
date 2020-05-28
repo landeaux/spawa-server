86d1de3f32ea57fc070c4d38869ffe8960f0392e
landeaux <pluckedowl@gmail.com>
2020-05-27 20:00:59 -0700

    Rebuild client for staging

--- server/public/js/about.5843bfe6.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about",attrs:{id:"view"}},[n("h1",[t._v("This is an about page")])])}],i=n("2877"),u={},r=Object(i["a"])(u,a,s,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=about.5843bfe6.js.map
